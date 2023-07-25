abc7adac8e95498a6248c5f095d5d5286f54288e
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-07-25 11:27:12 -0500

    update epi_api.js, script.js, index.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
-var apiKeyNPS = "2mkTMIQYFuS0De5GcmaGvYCMzkMaxFIpeVMaHNmV"
-var requestUrlNPS = `https://developer.nps.gov/api/v1/parks?stateCode=${State}&api_key=${apiKeyNPS}`
-    fetch(requestUrlNPS)
-    .then(function (response) {
-      return response.json(); 
-    })
-    .then(function(data){
-        console.log(data)
-    })
+$(document).ready(function(){
+  $("#state_selector").change(function(){
+    let state_choice = $(this).val();
+    $(document).trigger(get_park_aqi(state_choice))
+  })
+})
