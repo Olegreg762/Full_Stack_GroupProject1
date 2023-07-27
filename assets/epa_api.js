a88876a10a0c8fefb80b97179aa75780ca6bc4bf
J. Vargas <j.vargas@Js-MacBook-Pro.local>
2023-07-27 12:14:40 -0600

    style changes

--- assets/epa_api.js
+++ assets/epa_api.js
@@ -10,19 +10,22 @@ function get_park_aqi (input){
         .then(function(data){
             console.log(data)
             for( let r = 0; r < $("a").length; r++){
-                $(".Results-Container .result").remove()
+                $(".results-container .result").remove()
                 
             }
+            
+            
             for(let i = 0; i < data.data.length; i++){
                 if(data.data[i].designation !== "National Historic Trail"){
-                    $(`#result${i}`).append($(`#resultsList`));
-                    $(`<div class = "is-flex is-justify-content-space-between card box result">
+                    $(`#result${i}`).append($(`#searchResultsContainer`));
+                    $(`<div class = "is-flex is-justify-content: space-between, space-evenly, flex-direction: row-reverse card box result">
                         <a href="${data.data[i].url}" class="" id="park_url${i}">${data.data[i].fullName}</a>
                         <img src =${data.data[i].images[0].url} alt= "image of the national park" width = "200" height = "200">
                         <p>${data.data[i].description}</p>
                         <a href= ${data.data[i].directionsUrl}>Directions</a>
                         <span id="aqi_number${i}"></span>
-                        </div>`).insertAfter($(`#resultsList`));
+                        </div>`).insertAfter($(`#searchResultsContainer`));
+
                 const epa_url = `https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=${data.data[i].latitude}&longitude=${data.data[i].longitude}&distance=50&API_KEY=${epa_api}`
             fetch(epa_url)
                 .then(function(response){
