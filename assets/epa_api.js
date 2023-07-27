272582c0d6129ffa9dcfc71be55c29702a22ecd6
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-07-26 20:11:39 -0500

    delete results when new state chosen

--- assets/epa_api.js
+++ assets/epa_api.js
@@ -1,4 +1,3 @@
-let aqi = "";
 np_api_key = "qP8l7vELmn3qUfkuBwVesPUGqCAsmRHzcZU5XDOd"
 epa_api = "1B3DD7E3-FE66-40BD-9FF9-456CD1849D19"
 
@@ -9,25 +8,27 @@ function get_park_aqi (input){
             return response.json();
         })
         .then(function(data){
-            
+            for( let r = 0; r < $("a").length; r++){
+                $(".Results-Container a").remove()
+            }
             for(let i = 0; i < data.data.length; i++){
                 if(data.data[i].designation !== "National Historic Trail"){
-                    $(`#result${i}`).insertAfter($(`#resultsList`));
-                    $(`<a href="${data.data[i].url}" class="card box" id="park_url${i}">${data.data[i].fullName}<span id="aqi_number${i}"></span></a>`).insertAfter($(`#resultsList`));
-                const epa_url = `https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=${data.data[i].latitude}&longitude=${data.data[i].longitude}&distance=50&API_KEY=1B3DD7E3-FE66-40BD-9FF9-456CD1849D19`
+                    $(`#result${i}`).append($(`#resultsList`));
+                    $(`<a href="${data.data[i].url}" class="is-flex is-justify-content-space-between card box" id="park_url${i}">${data.data[i].fullName}<span id="aqi_number${i}"></span></a>`).insertAfter($(`#resultsList`));
+                const epa_url = `https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=${data.data[i].latitude}&longitude=${data.data[i].longitude}&distance=50&API_KEY=${epa_api}`
             fetch(epa_url)
                 .then(function(response){
                     return response.json();
                 })
                 .then(function(data){
                     if(data[0] == null){
-                        $(`#aqi_number${i}`).text(`: No Data Avaliable`)
+                        $(`#aqi_number${i}`).text(` No Data Avaliable`)
                     }else{
-                        $(`#aqi_number${i}`).text(`: Air Quality ${data[0].AQI} ${data[0].Category.Name}`)
+                        $(`#aqi_number${i}`).text(` Air Quality ${data[0].AQI} ${data[0].Category.Name}`)
                     }
                 })
             }}
         })
-  };
+    };
   
  
