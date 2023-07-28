6f2c6e48b619419063e71812731e9d5f7fb98cba
seantaylor1006 <seant1006@gmail.com>
2023-07-27 21:18:41 -0400

    changes to the meter

--- assets/epa_api.js
+++ assets/epa_api.js
@@ -1,5 +1,6 @@
 np_api_key = "qP8l7vELmn3qUfkuBwVesPUGqCAsmRHzcZU5XDOd"
-epa_api = "1B3DD7E3-FE66-40BD-9FF9-456CD1849D19"
+epa_api = "21DF2C57-020A-44AC-9755-F079CDA18E1C"//"1B3DD7E3-FE66-40BD-9FF9-456CD1849D19"
+
 
 function get_park_aqi (input){
     const url = `https://developer.nps.gov/api/v1/parks?stateCode=${input}&api_key=${np_api_key}`;
@@ -21,7 +22,7 @@ function get_park_aqi (input){
                         <img src =${data.data[i].images[0].url} class="mx-2" alt= "image of the national park" width = "200" height = "200">
                         <p class= "mx-2 has-text-white">${data.data[i].description}</p>
                         <a href= ${data.data[i].directionsUrl} class="mx-2 has-text-info-light">Directions </a>
-                        <span id="aqi_number${i}"></span>
+                        <span class="level-right mx-2 has-text-warning" id="aqi_number${i}"></span>
                         </div>`).insertAfter($(`#resultsList`));
                 const epa_url = `https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=${data.data[i].latitude}&longitude=${data.data[i].longitude}&distance=50&API_KEY=${epa_api}`
             fetch(epa_url)
@@ -33,7 +34,7 @@ function get_park_aqi (input){
                         $(`#aqi_number${i}`).text(` No Data Avaliable`)
                         plot_aqi(`aqi_number${i}`, "No Data") 
                     }else{
-                        // $(`#aqi_number${i}`).text(` Air Quality ${data[0].AQI} ${data[0].Category.Name}`)
+                         $(`#aqi_number${i}`).text(` Air Quality ${data[0].Category.Name}`)
                          plot_aqi(`aqi_number${i}`, data[0].AQI) 
                     }
                 })
